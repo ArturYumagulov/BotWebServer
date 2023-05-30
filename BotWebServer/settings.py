3b0218bb9e04da8bcf52559dd9b62a15d01994c9
Юмагулов Артур Ильгизович <YumagulovA@tranzit-oil.ru>
2023-05-30 11:42:25 +0300

    настроил статику

--- BotWebServer/settings.py
+++ BotWebServer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -29,7 +29,10 @@ SECRET_KEY = env.str("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['192.168.80.65', 'localhost']
 
 # Application definition
 
@@ -125,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
