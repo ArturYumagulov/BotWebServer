891503dd6c46a8cbb44640863a727e9a75594e47
Юмагулов Артур Ильгизович <YumagulovA@tranzit-oil.ru>
2023-06-05 15:35:39 +0300

    dumpdata. Подключил mysql

--- BotWebServer/settings.py
+++ BotWebServer/settings.py
@@ -85,11 +85,26 @@ WSGI_APPLICATION = "BotWebServer.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env('database'),
+        'USER': env('user'),
+        'HOST': env('host'),
+        'PORT': env('port'),
+        'PASSWORD': env('password'),
+        "OPTIONS": {
+            'charset': 'utf8',
+            # 'use_unicode': True,
+            }
+        }
 }
 
 
